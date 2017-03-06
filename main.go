2c7d302bab81c8e9653c62b8167b8c0713042a7a
Alexandre Dumont <alexandre@sogilis.com>
2017-03-06 13:58:33 +0100

    Add basic elasticsearch ping

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	elastic "gopkg.in/olivere/elastic.v5"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func main() {
+func helloAWS() {
 	sess, err := session.NewSession()
 	if err != nil {
 		panic(err)
@@ -33,4 +36,27 @@ func main() {
 			fmt.Println("    - Instance ID: ", *inst.InstanceId)
 		}
 	}
+}
+
+func main() {
+	// Starting with elastic.v5, you must pass a context to execute each service
+	ctx := context.Background()
+
+	// Obtain a client and connect to the default Elasticsearch installation
+	// on 127.0.0.1:9200. Of course you can configure your client to connect
+	// to other hosts and configure it in various other ways.
+	client, err := elastic.NewClient()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// Ping the Elasticsearch server to get e.g. the version number
+	info, code, err := client.Ping("http://127.0.0.1:9200").Do(ctx)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 }
